8ade6d055708eae2c7f73ee9fbb3ab7031c4920f
Renee <kroonrenee9@gmail.com>
2018-02-21 15:27:55 +0200

    lisasin/muutsin helloworld faili

--- paevakad/HelloWorld.java
+++ paevakad/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, it's Siim!");
-
+		System.out.println("Hello Siim!"); //muutsin seda rida
 	}
+	
 
 }
